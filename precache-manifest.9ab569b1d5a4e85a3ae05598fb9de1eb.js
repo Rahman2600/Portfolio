77311945f03ba58f3bfabbff977e2de0ffb35e18
rahman2600 <abdurrahmanmudasiru@yahoo.com>
2021-06-03 18:27:35 +0100

    Updates

---
+++ precache-manifest.9ab569b1d5a4e85a3ae05598fb9de1eb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbcefbfe24a05d1c5a88d2d274f879d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1878b660c35883e7600d",
+    "url": "/static/css/main.015e39d6.chunk.css"
+  },
+  {
+    "revision": "905ae1ea4cfc74a13487",
+    "url": "/static/js/2.b3099017.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.b3099017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1878b660c35883e7600d",
+    "url": "/static/js/main.32426106.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "0890a0842217a1e3d70ae9b7dae0ba60",
+    "url": "/static/media/hsbc.0890a084.png"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "409c2c29d21bf9b8185c237e7d995655",
+    "url": "/static/media/photo.409c2c29.png"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  }
+]);
